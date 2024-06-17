5cf47750ffb2ab8a30be1158402e029cc98caa61
jmbae <jmbae@codegrove.co.kr>
2024-06-17 17:40:27 +0900

    BookDetail 추가

--- BookShelf/BookShelf/BookDetailView.swift
+++ BookShelf/BookShelf/BookDetailView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct BookDetailView: View {
     @Binding var book: Book
-    
+    @State var showEditBookView = false
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            Text(book.title)
+            Image(book.largeCoverImageName)
+                .resizable()
+                .scaledToFit()
+                .shadow(radius: 10)
+                .padding()
+            Label(book.author, systemImage: "person.crop.rectangle")
+            Label("ISBN: \(book.isbn)", systemImage: "number")
+            Label("\(book.pages) pages", systemImage: "book")
+            Toggle("Read", isOn: .constant(book.isRead))
+            Button(action: { showEditBookView.toggle() }) {
+                Label("Edit", systemImage: "pencil")
+            }
+        }
+        .navigationTitle(book.title)
     }
 }
 
 #Preview {
-    BookDetailView(book: .constant(Book(title: "test", author: "test", isbn: "test", pages: 0)))
+    NavigationStack {
+        BookDetailView(book: .constant(Book.sampleBooks[0]))
+    }
 }
