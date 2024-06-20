feff73a0e90ab54d20168830ae536306798e403e
jmbae <jmbae@codegrove.co.kr>
2024-06-20 16:39:55 +0900

    k 오타 수정

--- SignUpForm2/SignUpForm2/Publisher+Retry.swift
+++ SignUpForm2/SignUpForm2/Publisher+Retry.swift
@@ -9,7 +9,7 @@ import Foundation
 import Combine
 
 extension Publisher {
-    func retry<T, E> (_ retryCount: Int, withBackkoff initialBackoff: Int, condition: ((E) -> Bool)? = nil) -> Publishers.TryCatch<Self, AnyPublisher<T, E>> where T == Self.Output, E == Self.Failure {
+    func retry<T, E> (_ retryCount: Int, withBackoff initialBackoff: Int, condition: ((E) -> Bool)? = nil) -> Publishers.TryCatch<Self, AnyPublisher<T, E>> where T == Self.Output, E == Self.Failure {
         return self.tryCatch { error -> AnyPublisher<T, E> in
             if condition?(error) == true {
                 var backOff = initialBackoff
