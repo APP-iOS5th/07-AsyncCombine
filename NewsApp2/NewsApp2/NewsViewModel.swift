f3f06dbe554d2c7295030f69f4dcabe2860dab29
jmbae <jmbae@codegrove.co.kr>
2024-06-21 16:46:55 +0900

    SearchNews 퍼블리셔 추가

--- NewsApp2/NewsApp2/NewsViewModel.swift
+++ NewsApp2/NewsApp2/NewsViewModel.swift
@@ -12,4 +12,28 @@ class NewsViewModel: ObservableObject {
     @Published var newsItems: [NewsItem] = []
     @Published var searchQuery = ""
     
+    private let newsService = NewsService()
+    private var cancellables = Set<AnyCancellable>()
+
+    private lazy var searchNewsPublisher: AnyPublisher<NewsResponse, Error> = {
+        $searchQuery
+            .debounce(for: .seconds(0.3), scheduler: RunLoop.main)
+            .removeDuplicates()
+            .filter { !$0.isEmpty }
+            .flatMap { query -> AnyPublisher<NewsResponse, Error> in
+                return self.newsService.searchNews(query: query, page: 1, itemsPerPage: 20)
+            }
+            .receive(on: DispatchQueue.main)
+            .share()
+            .eraseToAnyPublisher()
+    }()
+    
+    init() {
+        searchNewsPublisher
+            .catch { _ in Empty() }
+            .sink { [weak self] response in
+                self?.newsItems = response.items
+            }
+            .store(in: &cancellables)
+    }
 }
