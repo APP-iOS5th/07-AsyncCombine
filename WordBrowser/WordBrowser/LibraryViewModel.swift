2e2610faca33aa35e4fef8102c2e60853fd7bee3
jmbae <jmbae@codegrove.co.kr>
2024-06-24 13:39:14 +0900

    기본 뷰, 뷰모델 추가

--- WordBrowser/WordBrowser/LibraryViewModel.swift
+++ WordBrowser/WordBrowser/LibraryViewModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+
+class LibraryViewModel: ObservableObject {
+    @Published var searchText = ""
+    @Published var randomWord = "partially"
+    @Published var tips: [String] = ["Swift", "authentication", "authorization"]
+    @Published var favorites: [String] = ["stunning", "brilliant", "marvelous"]
+    
+    @Published var filteredTips = [String]()
+    @Published var filteredFavorites = [String]()
+}
