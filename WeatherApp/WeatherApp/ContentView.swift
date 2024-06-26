94ebe60deb9e2b9dcc13a963d590f65cec22df5e
jmbae <jmbae@codegrove.co.kr>
2024-06-26 13:08:20 +0900

    스티키 스크롤, 카드 커스텀 뷰 추가

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -7,6 +7,77 @@
 
 import SwiftUI
 
+//
+struct HeaderView: View {
+    var body: some View {
+        VStack {
+            Text("Seongnam-si")
+                .font(.system(size: 40))
+            Text("21°")
+                .font(.system(size: 110, weight: .thin))
+            Text("Partly Cloudy")
+                .font(.system(size: 25))
+            HStack {
+                Text("H:29°")
+                Text("L:15°")
+            }
+            .font(.system(size: 23))
+        }
+        .foregroundStyle(.white)
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct ScrollOffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+struct CardView<Content: View>: View {
+    var title: String
+    
+    let content: Content
+    
+    init(title: String, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.content = content()
+    }
+    
+    @State var scrollOffset: CGFloat = 0
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            GeometryReader { proxy in
+                Color.clear
+                    .preference(key: ScrollOffsetKey.self, value: proxy.frame(in: .global).minY)
+                    .onAppear {
+                        self.scrollOffset = proxy.frame(in: .global).minY
+                    }
+                    .onChange(of: proxy.frame(in: .global).minY) { old, newValue in
+                        self.scrollOffset = newValue
+                        print("\(title) changed by: \(newValue)")
+                    }
+            }
+            .frame(height: 0)
+            Text(title)
+                .font(.title)
+                .bold()
+            content
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        .background(
+            .ultraThinMaterial,
+            in: RoundedRectangle(cornerRadius: 20, style: .continuous)
+        )
+        .opacity(scrollOffset < 250 ? 0 : min(1,((scrollOffset - 250) / 250.0)))
+        .shadow(radius: 10)
+    }
+}
+
 struct ContentView: View {
     @StateObject var viewModel = WeatherViewModel()
     
@@ -14,41 +85,47 @@ struct ContentView: View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
             ScrollView {
-                VStack {
-                    Text("Seongnam-si")
-                        .font(.system(size: 40))
-                    Text("21°")
-                        .font(.system(size: 110, weight: .thin))
-                    Text("Partly Cloudy")
-                        .font(.system(size: 25))
-                    HStack {
-                        Text("H:29°")
-                        Text("L:15°")
-                    }
-                    .font(.system(size: 23))
-                    Text("")
-                        .padding(.top)
-                    Text("Cloudy conditions from 1AM-9AM, with showers expected at 9AM.")
-                    ScrollView(.horizontal) {
-                        LazyHStack {
-                            ForEach(0..<10) { index in
-                                VStack {
-                                    Text("\(index)PM")
-                                    Spacer()
-                                    Image(systemName: "cloud.fill")
-                                        .font(.largeTitle)
-                                    Spacer()
-                                    Text("21°")
-                                        .font(.system(size: 21, weight: .semibold))
+                LazyVStack(pinnedViews: [.sectionHeaders]) {
+                    Section {
+                        CardView(title: "TEST") {
+                            Text("Cloudy conditions from 1AM-9AM, with showers expected at 9AM.")
+                            Text("Cloudy conditions from 1AM-9AM, with showers expected at 9AM.")
+                            
+                        }
+                        
+                        CardView(title: "TEST1") {
+                            ScrollView(.horizontal) {
+                                LazyHStack {
+                                    ForEach(0..<10) { index in
+                                        VStack {
+                                            Text("\(index)PM")
+                                            Spacer()
+                                            Image(systemName: "cloud.fill")
+                                                .font(.largeTitle)
+                                            Spacer()
+                                            Text("21°")
+                                                .font(.system(size: 21, weight: .semibold))
+                                        }
+                                    }
                                 }
                             }
+                            .frame(height: 120)
                         }
-                    }
-                    .frame(height: 120)
-                    
-                    Section {
+                        
+                        
+                        HStack {
+                            Label("10-DAY FORCAST", systemImage: "calendar")
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        CardView(title: "TEST2") {
+                            Text("Cloudy conditions from 1AM-9AM, with showers expected at 9AM.")
+                            Text("Cloudy conditions from 1AM-9AM, with showers expected at 9AM.")
+                            
+                        }
+                        
                         VStack(alignment: .leading) {
-                            ForEach(0..<10) { index in
+                            ForEach(0..<100) { index in
                                 VStack {
                                     HStack {
                                         Text("Mon")
@@ -66,13 +143,11 @@ struct ContentView: View {
                                 .frame(maxWidth: .infinity)
                             }
                         }
+                        
                     } header: {
-                        HStack {
-                            Label("10-DAY FORCAST", systemImage: "calendar")
-                            Spacer()
-                        }
+                        HeaderView()
+                            .padding(.bottom, 20)
                     }
-                    .frame(maxWidth: .infinity)
                 }
                 .foregroundStyle(.white)
                 .padding()
